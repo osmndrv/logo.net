4c20f4e0ec96ade57ce7205762ade912b9bbbabf
osmndrv <ogabeksamandarov42@gmail.com>
2024-05-02 18:17:19 +0500

    logo.net

---
+++ script.js
@@ -0,0 +1,7 @@
+const moon = document.querySelector('.moon');
+const sun = document.querySelector('.sun');
+const body = document.querySelector('.body')
+
+sun.addEventListener('click', function(){
+    body.
+})
---
+++ service.js
@@ -0,0 +1,9 @@
+const moon = document.querySelector('.moon');
+const sun = document.querySelector('.sun');
+const body = document.querySelector('.body');
+moon.addEventListener('click', function(){
+    body.style.background = '#1a1a1a'
+})
+sun.addEventListener('click', function(){
+    body.style.background = 'white'
+})
